e13e1ba39c37268922cd656b0d959fe52dd917e4
man.nv <nguyenvanman1505@gmail.com>
2023-01-06 16:55:41 +0700

    Update container develop script

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -15,7 +15,7 @@ const prodConfig = {
       name: "marketing",
       filename: "remoteEntry.js",
       exposes: {
-        "./MarketingApp": "./src/bootstrap.js"
+        "./MarketingApp": "./src/bootstrap"
       },
       shared: packageJson.dependencies
     })
